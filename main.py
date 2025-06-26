425e41b846f613e4cf4dadee67d8ba6f2ac15aa6
max <max.bloechlinger.job@gmail.com>
2025-06-26 15:12:09 +0200

    added player as triangle

--- main.py
+++ main.py
@@ -1,24 +1,35 @@
-# this allows us to use code from
-# the open-source pygame library
-# throughout this file
 import pygame
 from constants import *
+from player import *
 
 def main():
-	pygame.init()
+    print("Starting Asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
 
-	screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+    pygame.init()
 
-	while True:
-		for event in pygame.event.get():
-    			if event.type == pygame.QUIT:
-        			return
+    clock = pygame.time.Clock()
+    dt = 0
 
-		screen.fill("black")
-		pygame.display.flip()
-	print("Starting Asteroids!")
-	print(f"Screen width: {SCREEN_WIDTH}")
-	print(f"Screen height: {SCREEN_HEIGHT}")
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+
+    player = Player(x, y)
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+        screen.fill("black")
+
+        # Draw the player
+        player.draw(screen)
+
+        pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
