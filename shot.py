cbbc1e9f4f270973169f336eb22bae94a29e144f
max <max.bloechlinger.job@gmail.com>
2025-06-27 11:16:06 +0200

    changed color of shot

--- shot.py
+++ shot.py
@@ -7,7 +7,7 @@ class Shot(CircleShape):
         super().__init__(x, y, SHOT_RADIUS)
     
     def draw(self, screen):
-        pygame.draw.circle(screen, "white", self.position, self.radius, self.radius)
+        pygame.draw.circle(screen, "purple", self.position, self.radius, self.radius)
     
     def update(self, dt):
         self.position += self.velocity*dt
